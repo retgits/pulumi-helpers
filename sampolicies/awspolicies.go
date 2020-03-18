e17b5d2a9f6178ace1df991becdd698e53c87572
retgits <lstigter@gmail.com>
2020-03-18 14:24:48 -0700

    feat: add AddExecuteAPI

--- sampolicies/awspolicies.go
+++ sampolicies/awspolicies.go
@@ -1,5 +1,17 @@
 package sampolicies
 
+// AddExecuteAPI allows the IAM role to execute API invocations
+func (f *Factory) AddExecuteAPI() {
+	policy := `{ "Action": "execute-api:Invoke", "Resource": "execute-api:/*", "Principal": "*", "Effect": "Allow" }`
+	f.policies = append(f.policies, policy)
+}
+
+// AddAssumeRoleLambda allows AWS Lambda to assume the role and use AWS services
+func (f *Factory) AddAssumeRoleLambda() {
+	policy := `{ "Action": "sts:AssumeRole", "Principal": { "Service": "lambda.amazonaws.com" }, "Effect": "Allow" }`
+	f.policies = append(f.policies, policy)
+}
+
 // AssumeRoleLambda returns an IAM policy document that allows the IAM role to be assumed by AWS Lambda
 func AssumeRoleLambda() string {
 	return `{ "Version": "2012-10-17", "Statement": [ { "Action": "sts:AssumeRole", "Principal": { "Service": "lambda.amazonaws.com" }, "Effect": "Allow" } ] }`
