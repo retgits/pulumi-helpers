b5530175ae6b038836e652f02f9f0cfa4cc4d1c4
retgits <lstigter@gmail.com>
2020-03-19 10:36:21 -0700

    feat: add GetGatewayResource

---
+++ gateway/gateway.go
@@ -0,0 +1,41 @@
+// Package gateway interacts with the AWS API Gateway resources built
+// with the Pulumi SDK
+package gateway
+
+import (
+	"fmt"
+
+	"github.com/pulumi/pulumi-aws/sdk/go/aws/apigateway"
+	"github.com/pulumi/pulumi/sdk/go/pulumi"
+)
+
+// MustGetGatewayResource is like GetGatewayResource but will panic when the resource is not found, or an error occurs
+func MustGetGatewayResource(ctx *pulumi.Context, gw *apigateway.RestApi, path string) *apigateway.LookupResourceResult {
+	resource, err := GetGatewayResource(ctx, gw, path)
+	if err != nil {
+		panic(err)
+	}
+
+	if resource == nil {
+		panic(fmt.Errorf("resource %s does not exist in gateway", path))
+	}
+
+	return resource
+}
+
+// GetGatewayResource gets the API resource based on the path. If no resource can be found
+// an error is thrown.
+func GetGatewayResource(ctx *pulumi.Context, gw *apigateway.RestApi, path string) (*apigateway.LookupResourceResult, error) {
+	c := make(chan string)
+	gw.ID().ToStringOutput().ApplyString(func(id string) string {
+		c <- id
+		return id
+	})
+
+	restID := <-c
+
+	return apigateway.LookupResource(ctx, &apigateway.LookupResourceArgs{
+		RestApiId: restID,
+		Path:      path,
+	})
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/retgits/pulumi-helpers
 
 go 1.14
+
+require (
+	github.com/pulumi/pulumi v1.13.0
+	github.com/pulumi/pulumi-aws v1.27.0
+)
