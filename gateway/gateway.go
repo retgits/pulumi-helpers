92e77ad6a0577db8c0b0f9c7ec0db952ec166386
retgits <lstigter@gmail.com>
2020-03-19 12:18:21 -0700

    chore: update GetGatewayResource

--- gateway/gateway.go
+++ gateway/gateway.go
@@ -10,8 +10,8 @@ import (
 )
 
 // MustGetGatewayResource is like GetGatewayResource but will panic when the resource is not found, or an error occurs
-func MustGetGatewayResource(ctx *pulumi.Context, gw *apigateway.RestApi, path string) *apigateway.LookupResourceResult {
-	resource, err := GetGatewayResource(ctx, gw, path)
+func MustGetGatewayResource(ctx *pulumi.Context, restID string, path string) *apigateway.LookupResourceResult {
+	resource, err := GetGatewayResource(ctx, restID, path)
 	if err != nil {
 		panic(err)
 	}
@@ -25,22 +25,7 @@ func MustGetGatewayResource(ctx *pulumi.Context, gw *apigateway.RestApi, path st
 
 // GetGatewayResource gets the API resource based on the path. If no resource can be found
 // an error is thrown.
-func GetGatewayResource(ctx *pulumi.Context, gw *apigateway.RestApi, path string) (*apigateway.LookupResourceResult, error) {
-	// Return a mock result for preview and dry run
-	if ctx.DryRun() {
-		return &apigateway.LookupResourceResult{
-			Id: "1",
-		}, nil
-	}
-
-	c := make(chan string)
-	gw.ID().ToStringOutput().ApplyString(func(id string) string {
-		c <- id
-		return id
-	})
-
-	restID := <-c
-
+func GetGatewayResource(ctx *pulumi.Context, restID string, path string) (*apigateway.LookupResourceResult, error) {
 	return apigateway.LookupResource(ctx, &apigateway.LookupResourceArgs{
 		RestApiId: restID,
 		Path:      path,
